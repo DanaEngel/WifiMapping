8976b33c23abb318c56426ebe34f75a26d4ae512
ennagrigor <ennagrigor@DESKTOP-VN5OF84>
2017-11-22 14:20:29 +0200

    Adding jar files for kml

--- WifiMapping/src/projectMap/TestWifi.java
+++ WifiMapping/src/projectMap/TestWifi.java
@@ -2,8 +2,8 @@ package projectMap;
 
 public class TestWifi {
 	public static void main(String[] args) {
-		String readFromFilePath = "C:\\Users\\dana\\Desktop\\files";
-		String writeToFilePath = "C:\\Users\\dana\\Desktop\\result.csv";
+		String readFromFilePath = "C:\\Users\\ennagrigor\\Desktop\\files";
+		String writeToFilePath = "C:\\Users\\ennagrigor\\Desktop\\result.csv";
 		WifiMapping wifiMapping = new WifiMapping();
 		wifiMapping.readFromFolderAndBuildCsv(readFromFilePath, writeToFilePath);
 		System.out.println("Csv file created at path: " + writeToFilePath);
