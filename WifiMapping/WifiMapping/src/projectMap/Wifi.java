eb15c56618c84c6d1964642378e1595101d18154
Enna Grigor <33618354+ennagrigor@users.noreply.github.com>
2017-11-24 00:24:11 +0200

    added java docs

---
+++ WifiMapping/WifiMapping/src/projectMap/Wifi.java
@@ -0,0 +1,143 @@
+package projectMap;
+
+/** This class represents all of the relevant information we need to fetch from
+ * the files.
+ * 
+ * @author Enna and Dana
+ */
+public class Wifi implements Comparable<Wifi> {
+	private String Time;
+	private String ID;
+	private String Lat;
+	private String Lon;
+	private String Alt;
+	private String SSID;
+	private String MAC;
+	private String Frequency;
+	private String Signal;
+
+	/**
+	 * This is the constructor
+	 * 
+	 * @param Time
+	 * @param ID
+	 * @param Lat
+	 * @param Lon
+	 * @param Alt
+	 * @param SSID
+	 * @param MAC
+	 * @param Frequency
+	 * @param Signal
+	 **/
+	public Wifi(String Time, String ID, String Lat, String Lon, String Alt, String SSID, String MAC, String Frequency,
+			String Signal) {
+		this.Time = Time;
+		this.ID = ID;
+		this.Lat = Lat;
+		this.Lon = Lon;
+		this.Alt = Alt;
+		this.SSID = SSID;
+		this.MAC = MAC;
+		this.Frequency = Frequency;
+		this.Signal = Signal;
+	}
+
+	/**
+	 * This is the the toString that creates a new string with the relevant
+	 * information.
+	 */
+	public String toString() {
+		String str = SSID + ',' + MAC + ',' + Frequency + ',' + Signal;
+		return str;
+	}
+
+	public String getTime() {
+		return Time;
+	}
+
+	public void setTime(String time) {
+		Time = time;
+	}
+
+	public String getID() {
+		return ID;
+	}
+
+	public void setID(String iD) {
+		ID = iD;
+	}
+
+	public String getLat() {
+		return Lat;
+	}
+
+	public void setLat(String lat) {
+		Lat = lat;
+	}
+
+	public String getLon() {
+		return Lon;
+	}
+
+	public void setLon(String lon) {
+		Lon = lon;
+	}
+
+	public String getAlt() {
+		return Alt;
+	}
+
+	public void setAlt(String alt) {
+		Alt = alt;
+	}
+
+	public String getSSID() {
+		return SSID;
+	}
+
+	public void setSSID(String sSID) {
+		SSID = sSID;
+	}
+
+	public String getMAC() {
+		return MAC;
+	}
+
+	public void setMAC(String mAC) {
+		MAC = mAC;
+	}
+
+	public String getFrequency() {
+		return Frequency;
+	}
+
+	public void setFrequency(String Frequency) {
+		this.Frequency = Frequency;
+	}
+
+	public String getSignal() {
+		return Signal;
+	}
+
+	public void setSignal(String Signal) {
+		this.Signal = Signal;
+	}
+
+	/**
+	 * This function compares each signal (the strength of wifi) and returns -1,0,1
+	 * according to whether it is stronger, the same or weaker.
+	 */
+	@Override
+	public int compareTo(Wifi o) {
+		int currentSignal = Integer.parseInt(this.Signal);
+		int otherSignal = Integer.parseInt(o.Signal);
+
+		if (currentSignal < otherSignal) {
+			return -1;
+		} else if (currentSignal == otherSignal) {
+			return 0;
+		} else {
+			return 1;
+		}
+	}
+}
---
+++ WifiMapping/WifiMapping/src/projectMap/WifiPoint.java
@@ -0,0 +1,141 @@
+package projectMap;
+
+/** This class represents all of the relevant information we need to fetch from
+ * the files.
+ * 
+ * @author Enna and Dana
+ */
+public class WifiPoint implements Comparable<WifiPoint> {
+	private String ssid;
+	private String mac;
+	private String frequency;
+	private String signal;
+	private String id;
+	private String lat;
+	private String lon;
+	private String alt;
+	private String time;
+
+	/**
+	 * THis is the constructor of the class.
+	 * 
+	 * @param ssid
+	 * @param mac
+	 * @param frequency
+	 * @param signal
+	 */
+	public WifiPoint(String ssid, String mac, String frequency, String signal) {
+		this.ssid = ssid;
+		this.mac = mac;
+		this.frequency = frequency;
+		this.signal = signal;
+	}
+
+	public String getSsid() {
+		return ssid;
+	}
+
+	public void setSsid(String ssid) {
+		this.ssid = ssid;
+	}
+
+	public String getMac() {
+		return mac;
+	}
+
+	public void setMac(String mac) {
+		this.mac = mac;
+	}
+
+	public String getFrequency() {
+		return frequency;
+	}
+
+	public void setFrequency(String frequency) {
+		this.frequency = frequency;
+	}
+
+	public String getSignal() {
+		return signal;
+	}
+
+	public void setSignal(String signal) {
+		this.signal = signal;
+	}
+
+	public String getId() {
+		return id;
+	}
+
+	public void setId(String id) {
+		this.id = id;
+	}
+
+	public String getLat() {
+		return lat;
+	}
+
+	public void setLat(String lat) {
+		this.lat = lat;
+	}
+
+	public String getLon() {
+		return lon;
+	}
+
+	public void setLon(String lon) {
+		this.lon = lon;
+	}
+
+	public String getAlt() {
+		return alt;
+	}
+
+	public void setAlt(String alt) {
+		this.alt = alt;
+	}
+
+	public String getTime() {
+		return time;
+	}
+
+	public void setTime(String time) {
+		this.time = time;
+	}
+
+	public void setSharedInfo(String lat, String lon, String alt, String time, String id) {
+		this.lat = lat;
+		this.lon = lon;
+		this.alt = alt;
+		this.time = time;
+		this.id = id;
+	}
+
+	/**
+	 * This is the the toString that creates a new string with the relevant
+	 * information.
+	 */
+	@Override
+	public String toString() {
+		String str = ssid + ',' + mac + ',' + frequency + ',' + signal;
+		return str;
+	}
+
+	/**
+	 * This function compares each signal (the strength of wifi) and returns -1,0,1
+	 * according to whether it is stronger, the same or weaker.
+	 */
+	@Override
+	public int compareTo(WifiPoint o) {
+		int currentSignal = Integer.parseInt(this.signal);
+		int otherSignal = Integer.parseInt(o.signal);
+
+		if (currentSignal < otherSignal) {
+			return -1;
+		} else if (currentSignal == otherSignal) {
+			return 0;
+		} else {
+			return 1;
+		}
+	}
+}
